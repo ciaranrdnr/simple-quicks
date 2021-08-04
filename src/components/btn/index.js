1a4828f687a32eae0b94248a5c45901f16b1acaf
ciaranrdnr <ciaranurdenara@gmail.com>
2021-08-04 20:34:55 +0700

    fixed hidden button

--- src/components/btn/index.js
+++ src/components/btn/index.js
@@ -1,7 +1,7 @@
 const Btn = (props) => {
   return (
     <button
-      className={`btn-regular ${props.color} ripple-${props.ripple} ${props.hide} ${props.ph}`}
+      className={`btn-regular ${props.color} ripple-${props.ripple} ${props.title}`}
       id={props.id}
       onClick={props.onClick}
     >
